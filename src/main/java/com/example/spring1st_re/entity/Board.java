73d79cdaa189d57f4e493c7d82a5116395c9f89e
EUIJOON <euijoonism@gmail.com>
2023-01-03 00:19:26 +0900

    [엔티티] 초기세팅

--- src/main/java/com/example/spring1st_re/entity/Board.java
+++ src/main/java/com/example/spring1st_re/entity/Board.java
@@ -1,5 +1,29 @@
 package com.example.spring1st_re.entity;
 
+import javax.persistence.Entity;
 
+@Entity
 public class Board {
+    private Long id;
+    private String title;
+    private String writer;
+    private String password;
+    private String content;
+
+    public Board() {} // 기본 생성자는 반드시 있어야 한다.
+
+    public Board(String title, String writer, String password, String content) {
+        this.title = title;
+        this.writer = writer;
+        this.password = password;
+        this.content = content;
+    } // 게시판 생성
+
+    public void update(String title, String writer, String content) {
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+    }
+
+
 }
