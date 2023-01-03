2eb302af30180cc31c72d959943d208346ac3719
EUIJOON <euijoonism@gmail.com>
2023-01-03 12:55:04 +0900

    [컨트롤러] GetMapping 코드 작업

--- src/main/java/com/example/spring1st_re/controller/BoardController.java
+++ src/main/java/com/example/spring1st_re/controller/BoardController.java
@@ -1,12 +1,10 @@
 package com.example.spring1st_re.controller;
 
+import com.example.spring1st_re.dto.BoardResponse;
 import com.example.spring1st_re.dto.CreateBoardRequest;
 import com.example.spring1st_re.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +15,12 @@ public class BoardController {
     //CreateBoardRequest 의 값을 전달 받음.
     public void createBoard(@RequestBody CreateBoardRequest createBoardRequest) {
         boardService.createBoard(createBoardRequest);
+    }
+
+    ///api/posts/1
+    @GetMapping("/api/posts/{boardId}")
+    public BoardResponse getBoard(@PathVariable Long boardId) {
+      return boardService.getBoard(boardId);
     }
 
 }
