6a0c2f5bf21eb893411e5a94b9d4e3c59036cd4b
EUIJOON <euijoonism@gmail.com>
2023-01-03 20:42:44 +0900

    [리포지토리] 게시글 정렬

--- src/main/java/com/example/spring1st_re/repository/BoardRepository.java
+++ src/main/java/com/example/spring1st_re/repository/BoardRepository.java
@@ -3,6 +3,10 @@ package com.example.spring1st_re.repository;
 import com.example.spring1st_re.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findAllByOrderByCreatedAtDesc();
 
 }
