c52ec0a52f409b23e484c4f38b18285815112960
EUIJOON <euijoonism@gmail.com>
2023-01-03 12:54:27 +0900

    [DTO] BoardResponse 작성

--- src/main/java/com/example/spring1st_re/dto/BoardResponse.java
+++ src/main/java/com/example/spring1st_re/dto/BoardResponse.java
@@ -1,4 +1,19 @@
 package com.example.spring1st_re.dto;
 
+import com.example.spring1st_re.entity.Board;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class BoardResponse {
+    private String title;
+    private String writer;
+    private String content;
+
+    public BoardResponse(Board board) {
+        this.title = board.getTitle();
+        this.writer = board.getWriter();
+        this.content = board.getContent();
+    }
 }
