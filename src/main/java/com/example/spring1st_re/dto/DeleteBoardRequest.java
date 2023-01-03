87c635790e71c6104e2bb021bd03720b34c86460
EUIJOON <euijoonism@gmail.com>
2023-01-03 19:25:59 +0900

    [DTO] 게시글 삭제 class DeleteBoardRequest 작성

--- src/main/java/com/example/spring1st_re/dto/DeleteBoardRequest.java
+++ src/main/java/com/example/spring1st_re/dto/DeleteBoardRequest.java
@@ -1,4 +1,16 @@
 package com.example.spring1st_re.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class DeleteBoardRequest {
+
+    private String password;
+
+    public DeleteBoardRequest(String password) {
+        this.password = password;
+    }
 }
+
