afceef175ed636640cbab5b03819fa16dfdb30c4
EUIJOON <euijoonism@gmail.com>
2023-01-03 16:10:43 +0900

    [서비스] 게시글 수정 로직 생성 + 비밀번호 유효성 검사 조건 추가 (Board도 미리 만들어놨음)

--- src/main/java/com/example/spring1st_re/service/BoardService.java
+++ src/main/java/com/example/spring1st_re/service/BoardService.java
@@ -2,6 +2,7 @@ package com.example.spring1st_re.service;
 
 import com.example.spring1st_re.dto.BoardResponse;
 import com.example.spring1st_re.dto.CreateBoardRequest;
+import com.example.spring1st_re.dto.UpdateBoardRequest;
 import com.example.spring1st_re.entity.Board;
 import com.example.spring1st_re.repository.BoardRepository;
 import org.springframework.stereotype.Service;
@@ -25,11 +26,27 @@ public class BoardService {
         Board board = new Board(createBoardRequest.getTitle(), createBoardRequest.getWriter(), createBoardRequest.getPassword(), createBoardRequest.getContent());
         boardRepository.save(board);
     }
+
     @Transactional
     // 게시글 조회 로직
     public BoardResponse getBoard(Long boardId) {
-       Board board = boardRepository.findById(boardId).orElseThrow(() -> new IllegalArgumentException("id 없음"));
+        Board board = boardRepository.findById(boardId).orElseThrow(() -> new IllegalArgumentException("id 없음"));
         return new BoardResponse(board);
     }
-}
 
+    // 게시글 수정 로직
+    @Transactional
+    public void updateBoard(Long boardId, UpdateBoardRequest updateBoardRequest) {
+        Board boardSaved = boardRepository.findById(boardId).orElseThrow(() -> new IllegalArgumentException("id 없음"));
+    //    boardSaved.update(updateBoardRequest.getTitle(), updateBoardRequest.getWriter(), updateBoardRequest.getContent());
+    //  추가요건 : 수정요청 시 수정 데이터와 비밀번호를 함께 보내서 서버에서 비밀번호 일치 여부 확인 후 업데이트 해라.
+    // 이게 앞서 넣어놓은(Board 에다가) 비밀번호 유효성 검사.
+        if(boardSaved.isValidPassword(updateBoardRequest.getPassword())) {
+            boardSaved.update(updateBoardRequest.getTitle(), updateBoardRequest.getWriter(), updateBoardRequest.getContent());
+            boardRepository.save(boardSaved);
+        } else {
+            throw new IllegalArgumentException("패스워드가 틀렸습니다!");
+        }
+
+    }
+}
