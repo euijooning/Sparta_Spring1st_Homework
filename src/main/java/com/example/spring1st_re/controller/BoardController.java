a376a78c28b0c29be96e9c4840abc71eb75bf1fb
EUIJOON <euijoonism@gmail.com>
2023-01-03 11:35:48 +0900

    [컨트롤러] BoardController 생성 기능 작성

--- src/main/java/com/example/spring1st_re/controller/BoardController.java
+++ src/main/java/com/example/spring1st_re/controller/BoardController.java
@@ -1,4 +1,22 @@
 package com.example.spring1st_re.controller;
 
+import com.example.spring1st_re.dto.CreateBoardRequest;
+import com.example.spring1st_re.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class BoardController {
+    private final BoardService boardService;
+
+    @PostMapping("/api/posts")
+    //CreateBoardRequest 의 값을 전달 받음.
+    public void createBoard(@RequestBody CreateBoardRequest createBoardRequest) {
+        boardService.createBoard(createBoardRequest);
+    }
+
 }
