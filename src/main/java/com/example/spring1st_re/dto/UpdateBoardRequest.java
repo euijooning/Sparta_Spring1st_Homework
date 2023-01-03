f4f55b06ea2afd79c7ba4a1315fc164832fa3133
EUIJOON <euijoonism@gmail.com>
2023-01-03 16:11:57 +0900

    [DTO] 게시글 수정 - UpdateBoardRequest 내용 작성

--- src/main/java/com/example/spring1st_re/dto/UpdateBoardRequest.java
+++ src/main/java/com/example/spring1st_re/dto/UpdateBoardRequest.java
@@ -1,4 +1,20 @@
 package com.example.spring1st_re.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class UpdateBoardRequest {
+    private String title;
+    private String writer;
+    private String content;
+    private String password;
+
+    public UpdateBoardRequest(String title, String writer, String content, String password) {
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+        this.password = password;
+    }
 }
