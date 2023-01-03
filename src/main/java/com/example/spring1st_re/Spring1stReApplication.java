278735c5dcc1cc9bfc7e05b3cc66303f07b5db76
EUIJOON <euijoonism@gmail.com>
2023-01-03 16:16:40 +0900

    [Application] @EnableJpaAuditing 추가하여 생성, 수정시간 나오게끔 마무리.

--- src/main/java/com/example/spring1st_re/Spring1stReApplication.java
+++ src/main/java/com/example/spring1st_re/Spring1stReApplication.java
@@ -2,8 +2,10 @@ package com.example.spring1st_re;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Spring1stReApplication {
 
     public static void main(String[] args) {
