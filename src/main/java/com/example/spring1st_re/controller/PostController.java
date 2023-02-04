1ffa27ecaf83afb91485ccccb0acb534029bd4e5
EUIJOON <euijoonism@gmail.com>
2023-02-04 21:54:59 +0900

    [작업] 발견한 오타들 수정

--- src/main/java/com/example/spring1st_re/controller/PostController.java
+++ src/main/java/com/example/spring1st_re/controller/PostController.java
@@ -4,7 +4,7 @@ import com.example.spring1st_re.dto.PostResponse;
 import com.example.spring1st_re.dto.CreatePostRequest;
 import com.example.spring1st_re.dto.DeletePostRequest;
 import com.example.spring1st_re.dto.UpdatePostRequest;
-import com.example.spring1st_re.service.BoardService;
+import com.example.spring1st_re.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,37 +13,37 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class PostController {
-    private final BoardService boardService;
+    private final PostService postService;
 
     // 게시물 생성
     @PostMapping("/api/posts")
     //CreateBoardRequest 의 값을 전달 받음.
-    public void createBoard(@RequestBody CreatePostRequest createPostRequest) {
-        boardService.createBoard(createPostRequest);
+    public void createPost(@RequestBody CreatePostRequest createPostRequest) {
+        postService.createPost(createPostRequest);
     }
 
     // 전체 게시물 조회
     @GetMapping("/api/posts")
-    public List<PostResponse> getBoardList() {
-        return boardService.getBoardList();
+    public List<PostResponse> getPostList() {
+        return postService.getPostList();
     }
 
     // 게시물 1개 조회
     ///api/posts/1
-    @GetMapping("/api/posts/{boardId}")
-    public PostResponse getBoard(@PathVariable Long boardId) {
-      return boardService.getBoard(boardId);
+    @GetMapping("/api/posts/{postId}")
+    public PostResponse getPost(@PathVariable Long postId) {
+      return postService.getPost(postId);
     }
 
     // 게시물 1개 수정
-    @PutMapping("/api/posts/{boardId}")
-    public void updateBoard(@PathVariable Long boardId, @RequestBody UpdatePostRequest updatePostRequest) {
-        boardService.updateBoard(boardId, updatePostRequest);
+    @PutMapping("/api/posts/{postId}")
+    public void updateBoard(@PathVariable Long postId, @RequestBody UpdatePostRequest updatePostRequest) {
+        postService.updatePost(postId, updatePostRequest);
     }
 
     // 게시물 삭제
-    @DeleteMapping("/api/posts/{boardId}")
-    public void deleteBoard(@PathVariable Long boardId, @RequestBody DeletePostRequest deletePostRequest) {
-        boardService.deleteBoard(boardId, deletePostRequest);
+    @DeleteMapping("/api/posts/{postId}")
+    public void deletePost(@PathVariable Long postId, @RequestBody DeletePostRequest deletePostRequest) {
+        postService.deletePost(postId, deletePostRequest);
     }
 }
